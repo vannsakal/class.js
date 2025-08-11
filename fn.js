05bcbede7adc8a2f695a2bf2a2fcb2f257b8c1ad
vannsakal <sakal.suon@gmail.com>
2025-08-11 11:11:16 +0700

    Add fn.js

---
+++ fn.js
@@ -0,0 +1,10 @@
+// function printHeart(){
+//     console.log("<3");
+// }
+// printHeart();
+
+function rant(){
+    for(let i=0; i<3; i++){
+        console.log()
+    }
+}
---
+++ test.js
@@ -0,0 +1,99 @@
+/*
+1.
+let fname = "Suon";
+let lname = "Vannsakal"
+console.log("Hello, my name is", fname, lname);
+*/
+
+/*
+2.
+let a = 10;
+let b = 5;
+let c = 3;
+console.log(a*b + c)
+*/
+
+/*
+3.
+isSunny = false;
+if(isSunny){
+    console.log("Wear sunglasses")
+}else{
+    console.log("Take an umbrella")
+}
+*/
+
+/*
+4.
+num = [5,10,15,20,25];
+num.forEach(num=>{
+    if(num>10){
+    console.log(num)
+    }
+})
+*/
+
+/*
+const readlineSync = require("readline-sync");
+let fname = readlineSync.question("First name: ");
+console.log("first name: "+ fname);
+*/
+
+/*
+5.
+let book = { 
+    title: "Javascript Basics",
+    author: "Sreymeng",
+    pages: 120
+};
+console.log(`The book ${book.title} by ${book.author} has ${book.pages} pages.`);
+*/
+
+/*
+6.
+let num = 0;
+while(num<=20){
+    if(num%2 == 0){
+        console.log(num);
+    }
+    num++;
+}
+*/
+
+/*
+7.
+let num = 10;
+while(num>0){
+    console.log(num);
+    num--
+}
+*/
+
+/*
+8.
+let num = [1,2,3,4,5];
+num.forEach(num=>{
+    console.log(num*2);
+})
+*/ 
+
+/*
+10.
+let count = 0;
+let scores = [50,70,60,80,40,50.60,70];
+scores.forEach(scores=>{
+    if(scores>=60){
+        count++
+    }
+});
+console.log("Student passed: ", count)
+*/
+
+// let str = " JavaScript is fun! ";
+// let result= "";
+// for(let x of str){
+//     if(x != " "){
+//         result += x;
+//     }
+// }
+// console.log(result.toUpperCase());
